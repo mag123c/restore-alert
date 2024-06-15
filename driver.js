0f305a1b218f0af1be0c545715c0be6a40a1c1cb
mag123c <diehreo@gmail.com>
2024-06-15 21:26:55 +0900

    error handle -1

--- driver.js
+++ driver.js
@@ -1,21 +1,27 @@
-import { Builder, Browser, Capabilities } from "selenium-webdriver";
+import { Browser, Builder, Capabilities } from "selenium-webdriver";
 
 export async function connDriver(url) {
-    const chromeCapabilities = Capabilities.chrome();
-    chromeCapabilities.set('goog:chromeOptions', {
-        args: [
-            '--headless',
-            '--no-sandbox',
-            '--disable-dev-shm-usage',
-            '--disable-gpu',
-            '--window-size=1920,1080'
-        ]
-    });
+    try {
+        const chromeCapabilities = Capabilities.chrome();
+        chromeCapabilities.set('goog:chromeOptions', {
+            args: [
+                '--headless',
+                '--no-sandbox',
+                '--disable-dev-shm-usage',
+                '--disable-gpu',
+                '--window-size=1920,1080'
+            ]
+        });
 
-    let driver = await new Builder()
-        .forBrowser(Browser.CHROME)
-        .withCapabilities(chromeCapabilities)
-        .build();
-    await driver.get(url);
-    return driver;
+        let driver = await new Builder()
+            .forBrowser(Browser.CHROME)
+            .withCapabilities(chromeCapabilities)
+            .build();
+        await driver.get(url);
+        return driver;
+    }
+    catch (e) {
+        throw e;
+    }
+
 }
--- ironstein.js
+++ ironstein.js
@@ -6,10 +6,15 @@ import { By } from "selenium-webdriver";
  * @returns boolean
  */
 async function isBuyBtnIsActive(driver) {
-    const buyDiv = await driver.findElement(By.className("buy_btns holder pc   "));
-    const buyBtn = await buyDiv.findElement(By.xpath("./*"));
-    const btnText = await buyBtn.getText();
-    return !btnText.includes("품절");
+    try {
+        const buyDiv = await driver.findElement(By.className("buy_btns holder pc   "));
+        const buyBtn = await buyDiv.findElement(By.xpath("./*"));
+        const btnText = await buyBtn.getText();
+        return !btnText.includes("품절");
+    }
+    catch (e) {
+        throw new Error('구매 버튼을 찾을 수 없습니다.')
+    }
 }
 
 /**
@@ -19,11 +24,17 @@ async function isBuyBtnIsActive(driver) {
  * @returns 텍스트
  */
 async function getElementText(element, driver) {
-    let text = await element.getText().catch(() => "");
-    if (!text.trim()) {
-        text = await driver.executeScript("return arguments[0].innerText;", element);
+    try {
+        let text = await element.getText().catch(() => "");
+        if (!text.trim()) {
+            text = await driver.executeScript("return arguments[0].innerText;", element);
+        }
+        return text.trim();
     }
-    return text.trim();
+    catch (e) {
+        throw new Error('텍스트를 가져올 수 없습니다.');
+    }
+
 }
 
 /**
@@ -33,36 +44,47 @@ async function getElementText(element, driver) {
  * @returns number
  */
 async function isSelectedSize(driver, options) {
-    const optionsSpan = await driver.findElements(By.className("blocked margin-bottom-lg"));
-    const pricesSpan = await driver.findElements(By.className("no-margin blocked"));
+    try {
+        const optionsSpan = await driver.findElements(By.className("blocked margin-bottom-lg"));
+        const pricesSpan = await driver.findElements(By.className("no-margin blocked"));
 
-    for (let i = 0; i < optionsSpan.length; i++) {
-        const optionText = await getElementText(optionsSpan[i], driver);
-        const priceText = await getElementText(pricesSpan[i], driver);
+        for (let i = 0; i < optionsSpan.length; i++) {
+            const optionText = await getElementText(optionsSpan[i], driver);
+            const priceText = await getElementText(pricesSpan[i], driver);
 
-        if (optionText === options) {
-            return priceText.includes("품절") ? 0 : 1;
+            if (optionText === options) {
+                return priceText.includes("품절") ? 0 : 1;
+            }
         }
+
+        return -1;
+    }
+    catch (e) {
+        throw e;
     }
 
-    return -1;
 }
 
 export async function ironstein(driver, options) {
-    if (!await isBuyBtnIsActive(driver)) {
-        console.log("품절");
-        return;
+    try {
+        if (!await isBuyBtnIsActive(driver)) {
+            console.log("품절");
+            return;
+        }
+
+        const restock = await isSelectedSize(driver, options);
+
+        if (restock == -1) throw new Error('옵션을 찾을 수 없습니다.');
+
+        if (restock == 0) {
+            console.log(`${options} 품절`);
+            return;
+        }
+
+        //재입고 알림 구현
+        return true;
     }
-
-    const restock = await isSelectedSize(driver, options);
-
-    if (restock == -1) throw new Error('옵션을 찾을 수 없습니다.');
-
-    if (restock == 0) {
-        console.log(`${options} 품절`);
-        return;
+    catch (e) {
+        throw e;
     }
-
-    //재입고 알림 구현
-    return true;
 }
