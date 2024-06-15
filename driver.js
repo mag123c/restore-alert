5d0b2cbdfbcc8664c6573254203aa7d596a64576
JaeHo Jang <diehreo@gmail.com>
2024-06-15 21:23:07 +0900

    Update driver.js

--- driver.js
+++ driver.js
@@ -1,7 +1,21 @@
-import { Browser, Builder } from 'selenium-webdriver';
+import { Builder, Browser, Capabilities } from "selenium-webdriver";
 
 export async function connDriver(url) {
-    let driver = await new Builder().forBrowser(Browser.CHROME).build();
+    const chromeCapabilities = Capabilities.chrome();
+    chromeCapabilities.set('goog:chromeOptions', {
+        args: [
+            '--headless',
+            '--no-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-gpu',
+            '--window-size=1920,1080'
+        ]
+    });
+
+    let driver = await new Builder()
+        .forBrowser(Browser.CHROME)
+        .withCapabilities(chromeCapabilities)
+        .build();
     await driver.get(url);
     return driver;
 }
